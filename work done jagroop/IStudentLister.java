d2fed0bc445a9c13d16ff57433c597fe7c353f79
Roopchhina <jattchhina01@gmail.com>
2016-08-30 11:02:24 +1000

    Add files via upload

---
+++ work done jagroop/IStudentLister.java
@@ -0,0 +1,13 @@
+package datamanagement;
+
+
+/**
+ * @author jtulip
+ */
+
+public interface IStudentLister {
+
+    public void clearStudents();
+    
+public void addStudent(IStudent student);
+}
