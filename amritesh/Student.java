1220f0c4a17781a244a64dbba29ff83d81670771
amoudgil07 <amoudgil07@gmail.com>
2016-08-30 15:16:14 +1000

    Add files via upload

---
+++ amritesh/Student.java
@@ -0,0 +1,48 @@
+package datamanagement;
+
+/**
+ *
+ * @author 11541761
+ */
+public class Student implements IStudent 
+{
+    private Integer id; private String fn;
+            private String ln;
+private StudentUnitRecordList su;
+
+public Student( Integer id, String fn, String ln, StudentUnitRecordList su ) 
+{ this.id = id; this.fn = fn;
+        this.ln = ln;this.su = 
+        su == null ? new StudentUnitRecordList() : 
+                su;
+}
+//getting information from student
+    public Integer getID() { return this.id; 
+} public String getFirstName() { 
+return fn; }
+
+    public void setFirstName( String firstName )
+    { 
+this.fn = firstName; }
+
+public String getLastName()
+{ 
+    return ln; }
+    public void setLastName( String lastName ) { 
+
+        
+this.ln = lastName; }
+
+public void addUnitRecord( IStudentUnitRecord record )
+{ su.add(record); }
+        public IStudentUnitRecord getUnitRecord( String unitCode ) 
+        {
+for ( IStudentUnitRecord r : su ) 
+            if ( r.getUnitCode().equals(unitCode)) 
+return r; 
+
+return null;
+        
+}
+
+public StudentUnitRecordList getUnitRecords() { return su; }}
